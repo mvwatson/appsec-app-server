8a8c9090df15448dec49eb46a69573073e77489f
Michael <michael@michael.com>
2018-03-04 21:12:30 +1100

    Spring starter

---
+++ src/main/java/com/livingdataonline/appsecappserver/AppsecAppServerApplication.java
@@ -0,0 +1,12 @@
+package com.livingdataonline.appsecappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsecAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsecAppServerApplication.class, args);
+	}
+}
